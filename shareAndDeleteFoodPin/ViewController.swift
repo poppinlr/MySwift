c2545fa01fc7507752cd35af34671255b48ace77
沈卓晨 <poppinlr@vip.qq.com>
2015-07-02 13:16:08 +0800

    Initial Commit

---
+++ shareAndDeleteFoodPin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shareAndDeleteFoodPin
+//
+//  Created by 沈卓晨 on 15/7/2.
+//  Copyright (c) 2015年 poppinlr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
